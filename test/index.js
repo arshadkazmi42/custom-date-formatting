663bf901f041e441348e91ade8bd933c7f7b3aa9
Sachin <sac9798@gmail.com>
2018-10-01 02:51:14 +0530

    Updated index.js
    Finalised index.js file and mocha module checker passed all the tests.

--- test/index.js
+++ test/index.js
@@ -15,24 +15,26 @@ describe('#nextOrPrevDateCustomFormat', function() {
 
 describe('#nextOrPrevMinuteDateCustomFormat', function () {
   it('converts input date, adding some minute', function () {
-         nextOrPrevMinuteDateCustomFormat("YYYY-MM-DD HH:mm", 1).should.equal(moment().format('YYYY-MM-DD HH:mm:ss');
-     });
+         nextOrPrevMinuteDateCustomFormat("YYYY-MM-DD HH:mm", 0).should.equal(moment().format("YYYY-MM-DD HH:mm"));
+      
+});
 });
 
  describe('#nextOrPrevSecondDateCustomFormat', function () {
      it('converts input date, adding some second', function () {
-         nextOrPrevSecondDateCustomFormat("YYYY-MM-DD HH:mm:ss", 10).should.equal(moment().format('YYYY-MM-DD HH:mm:ss');
+         nextOrPrevSecondDateCustomFormat("YYYY-MM-DD HH:mm:ss", 0).should.equal(moment().format("YYYY-MM-DD HH:mm:ss"));
      });
  });
 
 describe('#customSourceDateFormatToCustomNewDateFormat', function () {
     it('converts custom input date to source format to required format', function () {
-        customSourceDateFormatToCustomNewDateFormat(moment().format('YYYY-MM-DD HH:mm:ss'), "YYYY-MM-DD HH:mm:ss", "dddd DD MMM YYYY").should.equal(moment().format('dddd DD MMM YYYY');
+        customSourceDateFormatToCustomNewDateFormat(moment().format("YYYY-MM-DD HH:mm:ss"), "YYYY-MM-DD HH:mm:ss", "dddd DD MMM YYYY").should.equal(moment().format("dddd DD MMM YYYY"));
     });
 });
 
 describe('#timeStampFromCurrentDateFormat', function () {
    it('converts custom input date and its format to timestamp', function () {
-       timeStampFromCurrentDateFormat(moment().format('DD MMM YYYY'), "DD MMM YYYY").should.equal(Date.now());
+       timeStampFromCurrentDateFormat(moment().format("DD MMM YYYY"), "DD MMM YYYY").should.equal(Date.parse(moment().format("DD MMM YYYY")));
    });
 });
+
